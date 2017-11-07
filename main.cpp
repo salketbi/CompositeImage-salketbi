887fd45c616185fba70e02097753aca086a285f0
salketbi@mail.csuchico.edu <salketbi@ecc-jaguar.csuchico.edu>
2017-11-06 22:31:11 -0800

    p03

---
+++ main.cpp
@@ -0,0 +1,38 @@
+// Step 1: Initialize file name array to store 10 filenames.
+// I will use Do WHILE Loop to iterate over 10 file names
+// and take input the file name from user and store it in the vector.
+// I also put condition to stop the loop, when user enters DONE. 
+// 
+
+// Step 2
+/**
+* This function will open files and check two things:
+* (1) the file exist or not and also check that a valid bitmap or not 
+* (2) check dimension of the files,
+* if there any error,it will give error.
+* This function will not return any thing and it takes string filename as parameter. 
+**/
+// void validateFilenames(string filename
+
+// Step 3
+/**
+ * This function will check the composite Image can be created or not on the basis of 
+* of the dimension of the atleast 2 images.
+* This function will not return any thing and it takes array of filename as parameter. 
+**/
+// void checkDimension(string [] filename) 
+
+
+// Step 4
+/**
+* This function will combine the valid images to single composite image.
+* It also calculate the average RGB values of all images for each pixel.
+* This function will not return any thing and it takes bitmap vector.
+**/
+//void combineImages(bitmap)
+
+
+// Step 5
+
+// Save the File using save() function as  composite-salketbi.bmp
+
