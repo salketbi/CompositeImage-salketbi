9f9b2663c7a664ee847b40d1f8c118659dbd5a25
salketbi@mail.csuchico.edu <salketbi@ecc-jaguar.csuchico.edu>
2017-11-13 15:41:39 -0800

    p03

--- main.cpp
+++ main.cpp
@@ -1,38 +1,162 @@
-// Step 1: Initialize file name array to store 10 filenames.
-// I will use Do WHILE Loop to iterate over 10 file names
-// and take input the file name from user and store it in the vector.
-// I also put condition to stop the loop, when user enters DONE. 
-// 
+#include<iostream>
+#include "bitmap.h"
+#include <vector>
+#include <string.h>
+#define MAX 10
 
-// Step 2
-/**
-* This function will open files and check two things:
-* (1) the file exist or not and also check that a valid bitmap or not 
-* (2) check dimension of the files,
-* if there any error,it will give error.
-* This function will not return any thing and it takes string filename as parameter. 
-**/
-// void validateFilenames(string filename
+using namespace std;
+bool checkDimension(vector <string>);
+void combineImages(vector <string>,int, int d[10][2] );
+int main()
+{
+string filename;
+vector <string> fileVector;
+string str = "DONE";
+bool flag;
+int i = 1;
+cout<<"Enter File Names with extension .bmp (Max. 10) (DONE to stop) . "<<endl;
+do
+{
+cout<<" File "<<i<< " : ";
+getline (cin,filename);
+if(filename != str)
+{
+fileVector.push_back(filename);
+}
+i++;
+} while(i<=MAX && filename != str);
 
-// Step 3
-/**
- * This function will check the composite Image can be created or not on the basis of 
-* of the dimension of the atleast 2 images.
-* This function will not return any thing and it takes array of filename as parameter. 
-**/
-// void checkDimension(string [] filename) 
+flag =  checkDimension(fileVector);
+if(flag)
+{
+cout<<"Composite Image created successfully."<<endl;
+}
+else
+{
+cout<<"Composite Image can't be created"<<endl;
+}
+return 0;
+}
+
+bool checkDimension(vector <string> fileVector)
+{
+int dimens[10][2];
+vector <string> validFiles;
+int count = 0;
+int valid = 1;
+bool flag;
+for (int i=0;i<fileVector.size();i++)
+{
+Bitmap image;
+vector <vector <Pixel> > bmp;
+int width = 0;  // width of image
+int height = 0;  // height of image
+image.open(fileVector[i]);
+bool validBmp = image.isImage();
+if ( validBmp == true )
+{
+bmp = image.toPixelMatrix();
+height = bmp.size();
+width = bmp[0].size();
+dimens[count][0] = width;
+dimens[count][1] = height;
+validFiles.push_back(fileVector[i]);
+count = count + 1;
+}
+}
 
 
-// Step 4
-/**
-* This function will combine the valid images to single composite image.
-* It also calculate the average RGB values of all images for each pixel.
-* This function will not return any thing and it takes bitmap vector.
-**/
-//void combineImages(bitmap)
+if(count<2)
+{
+flag = false;
+return flag;
+}
+
+if (count >= 2)
+{
+for(int i=0;i<count;i++)
+{
+if(dimens[0][0] == dimens[i+1][0] && dimens[0][1] == dimens[i+1][1])
+{
+flag = true;
+valid = valid + 1;
+}
+}
+}
+
+combineImages(validFiles,valid, dimens);
+
+for (int i = 1; i < valid; ++i)
+{
+std::cout << "Image "<<i <<" of "<<valid<<" done" <<std::endl;
+}
+
+return flag;
+}
 
 
-// Step 5
+void combineImages(vector <string> fileVector,int valid, int dimens[10][2] )
+{
+Bitmap image[valid];
+Pixel rgb[valid];
+vector < vector < vector<Pixel> > > bmp;
 
-// Save the File using save() function as  composite-salketbi.bmp
+Pixel px;
+Pixel avg_px;
+int rows,cols;
+int k = 0;
+int p = 0;
+cols = dimens[k][0];
+rows = dimens[k][1];
+
+for(k=0;k<valid;k++)
+{
+image[k].open(fileVector[k]);
+bmp.push_back(image[k].toPixelMatrix());
+}
+
+for(int i=0;i<rows;i++)
+{
+for(int j=0;j<cols;j++)
+{
+avg_px.red = 0;
+avg_px.green = 0;
+avg_px.blue = 0;
+px.red = 0;
+px.green = 0;
+px.blue = 0;
+
+for(p = 0; p<valid; p++)
+{
+rgb[p] = bmp[p][i][j];
+}
+
+for(p = 0; p<valid; p++)
+{
+px.red += rgb[p].red;
+}
+
+for(p = 0; p<valid; p++)
+{
+px.green += rgb[p].green;
+}
+
+for(p = 0; p<valid; p++)
+{
+px.blue += rgb[p].blue;
+}
+
+avg_px.red = px.red/valid;
+avg_px.green = px.blue/valid;
+avg_px.blue = px.blue/valid;
+bmp[p-1][i][j] = avg_px;
+}
+
+}
+
+image[k-1].fromPixelMatrix(bmp[k-1]);
+//save the updated original image as oldtimey.bmp
+image[k-1].save("composite-salketbi.bmp");
+
+}
 
